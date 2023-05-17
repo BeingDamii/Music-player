05a02cf5d2d55461906e138f5c151c628d05327d
Damii dennison <dwanedehydra@gmail.com>
2023-05-17 14:39:22 +0100

    created and animated scrollable text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Navbar from "./components/navbar";
 import Player from "./components/player";
+import ScrollableText from "./components/scrollableText";
 import Song from "./components/songs";
 import "./styles/app.scss";
 
 function App() {
   return (
     <div className="main-wrapper">
-      <Navbar/>
+      <Navbar />
       <Song />
       <Player />
+      <ScrollableText />
     </div>
   );
 }
---
+++ src/components/scrollableText.jsx
@@ -0,0 +1,14 @@
+const ScrollableText = () => {
+  return (
+    <div className="scrollableText-wrapper">
+      <div className="text-wrapper">
+        <h1 className="text">MY LOFI playlist</h1>
+      </div>
+      <div className="text-wrapper">
+        <h1 className="text other">MY LOFI playlist</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollableText;
