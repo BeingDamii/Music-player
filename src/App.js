d3128c5f2ecbe39eed3a75baf1718713e7bf6c9f
Damii dennison <dwanedehydra@gmail.com>
2023-05-17 16:46:37 +0100

    console errors fixed, songs data imported

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import Navbar from "./components/navbar";
 import Player from "./components/player";
 import ScrollableText from "./components/scrollableText";
 import Song from "./components/songs";
+import { useState } from "react";
 import musicData from "./resources/util";
 import "./styles/app.scss";
 
 function App() {
+  const [songs,setSongs] = useState(musicData());
   return (
     <div className="main-wrapper">
       <Navbar />
