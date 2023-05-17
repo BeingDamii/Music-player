69d0657ca772e9914bdee5d842aeba579f4760fc
Damii dennison <dwanedehydra@gmail.com>
2023-05-17 03:56:49 +0100

    styled song section for desktop

--- src/components/songs.jsx
+++ src/components/songs.jsx
@@ -1,10 +1,15 @@
-
+import Artwork from "../resources/artwork.png";
+import { DragIcon } from "../resources/icons";
 const Song = () => {
-    return (
-        <div className="song-wrapper">
-            <h1>hello song</h1>
-        </div>
-    );
-}
+  return (
+    <div className="song-wrapper">
+      <img src={Artwork} alt="demo-artwork" />
+      <div className="drag-instruction-div">
+        <DragIcon />
+        <h4>Drag song here to update player</h4>
+      </div>
+    </div>
+  );
+};
 
 export default Song;
