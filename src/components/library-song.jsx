95c83be6a2aa7f5a348e4edf3d9f6637202d6e7a
Damii dennison <dwanedehydra@gmail.com>
2023-05-20 16:13:47 +0100

    active songs dynamically updates

--- src/components/library-song.jsx
+++ src/components/library-song.jsx
@@ -12,11 +12,26 @@ const LibrarySong = ({
     const selectedSong = songs.filter((song) => song.id === id);
 
     setCurrentSong(selectedSong[0]);
-    console.log(isPlaying);
+    const updatedSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(updatedSongs);
+
     // setIsplaying(true)
 
     if (isPlaying) {
       const playPromise = audioRef.current.play();
+      console.log(audioRef.current);
       if (playPromise !== undefined) {
         playPromise.then((audio) => {
           audioRef.current.play();
@@ -26,7 +41,10 @@ const LibrarySong = ({
   }
 
   return (
-    <div className="library-song-wrapper" onClick={changeSong}>
+    <div
+      className={`library-song-wrapper ${song.active ? "active" : ""}`}
+      onClick={changeSong}
+    >
       <div>
         <img src={song.cover} alt="library song" />
       </div>
